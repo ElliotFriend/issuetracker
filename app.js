90f67fde06aa600bd24ea157172a3c19e3e2c567
Elliot J. Voris <elliot@voris.me>
2021-11-23 22:59:37 -0600

    Barebones of issue creation working.
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- app.js
+++ app.js
@@ -10,13 +10,12 @@ const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
 
+
 let app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
-
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,21 +23,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Sample front-end
 app.route('/:project/')
   .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/issue.html');
+    res.sendFile(`${process.cwd()}/views/issue.html`);
   });
 
 //Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(`${process.cwd()}/views/index.html`);
   });
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
@@ -48,7 +47,7 @@ app.use(function(req, res, next) {
 
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log(`Your app is listening on port ${listener.address().port}`);
   if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
     setTimeout(function () {
--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,51 @@
 'use strict';
 
+const mongoose          = require('mongoose')
+const issueSchema       = require('../issueSchema');
+let Issue = mongoose.model('Issue', issueSchema)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const createIssue = async (issueBody, done) => {
+  let issue = new Issue({
+    ...issueBody
+  })
+  issue.save( (err, data) => {
+    if (err) return console.log(err)
+    done(null, data)
+  })
+}
+
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
-  
+
     .get(function (req, res){
       let project = req.params.project;
-      
+
     })
-    
-    .post(function (req, res){
+
+    .post(async (req, res) => {
       let project = req.params.project;
-      
+      console.log(req.body)
+      let doc = await createIssue({
+        ...req.body,
+        project: project,
+      }, (err, data) => {
+        if (err) return console.log(err)
+        res.json({
+          ...data._doc
+        })
+      })
     })
-    
+
     .put(function (req, res){
       let project = req.params.project;
-      
+
     })
-    
+
     .delete(function (req, res){
       let project = req.params.project;
-      
+
     });
-    
+
 };
